7ee4a11a069d021b722f1071199f75f95a3995e2
Kamil <qkiz288@gmail.com>
2022-05-25 15:06:21 +0200

    Type guards

--- app/types/IntersectionType.js
+++ app/types/IntersectionType.js
@@ -3,5 +3,44 @@ const e1 = {
     privileges: ['drop-database'],
     startDate: new Date()
 };
-console.log(e1);
+function add(a, b) {
+    if (typeof a === 'string' || typeof b === 'string') { // type guard
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+console.log(add('Kamil', 'Kukielka'));
+function printEmployeeInfo(emp) {
+    console.log('Name: ' + emp.name);
+    if ('privileges' in emp) { // type guard
+        console.log('Privileges: ' + emp.privileges);
+    }
+    if ('startDate' in emp) { // type guard
+        console.log('Start Date: ' + emp.startDate);
+    }
+}
+console.log(printEmployeeInfo({ name: 'Kamil', startDate: new Date() }));
+class Car {
+    drive() {
+        console.log("Driving...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck...");
+    }
+    loadCargo(amount) {
+        console.log(`Loading ${amount} cargos....`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
 //# sourceMappingURL=IntersectionType.js.map
