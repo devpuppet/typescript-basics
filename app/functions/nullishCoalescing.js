4d4bf87d810d288348069448fb570505db2c6dbc
Kamil <qkiz288@gmail.com>
2022-05-30 12:08:45 +0200

    Nullish coalescing

---
+++ app/functions/nullishCoalescing.js
@@ -0,0 +1,7 @@
+const inputText = '';
+let storedData = inputText !== null && inputText !== void 0 ? inputText : 'Default';
+console.log("Empty input: " + storedData);
+const nullText = null;
+storedData = nullText !== null && nullText !== void 0 ? nullText : 'Default';
+console.log("null data: " + storedData);
+//# sourceMappingURL=nullishCoalescing.js.map
