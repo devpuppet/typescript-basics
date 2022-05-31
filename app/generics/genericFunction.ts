76f828225f93b4c1de424a0d54228a199ade29a6
Kamil <qkiz288@gmail.com>
2022-05-31 09:55:52 +0200

    additional generic function

--- app/generics/genericFunction.ts
+++ app/generics/genericFunction.ts
@@ -13,3 +13,20 @@ function correctMerge<T extends object, U extends object>(objA: T, objB: U) {
 const correctMergedObj = correctMerge({name: 'Max'}, {age: 30});
 console.log("Object with age = 30 (passed as object):");
 console.log(correctMergedObj);
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T) {
+    let description = 'Got no value.';
+    if (element.length === 1) {
+        description = 'Got 1 element';
+    } else if (element.length > 1) {
+        description = `Got ${element.length} elements`;
+    }
+    return [element, description];
+}
+
+console.log(countAndDescribe(['Kamil', 'Sara']));
+console.log(countAndDescribe('Abcdefg'));
