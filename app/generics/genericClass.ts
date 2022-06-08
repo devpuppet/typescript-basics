3cfd302b8ef64fcdfbdb27465ce7307de6cc6439
Kamil <qkiz288@gmail.com>
2022-06-08 16:38:54 +0200

    Generic class

---
+++ app/generics/genericClass.ts
@@ -0,0 +1,21 @@
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Kamil');
+textStorage.addItem('Sara');
+textStorage.removeItem('Kamil');
+console.log(textStorage.getItems());
